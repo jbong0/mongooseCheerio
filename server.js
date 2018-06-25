b2ac21839e988432082e9011dbfbfd1c0e825fff
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-06-25 14:33:29 -0700

    Styling

--- server.js
+++ server.js
@@ -2,14 +2,31 @@ var express = require("express")
 var exphbs = require("express-handlebars")
 var app = express()
 var bodyParser = require("body-parser")
+var path = require("path")
+var PORT = 3000
 
-const cheerio = require('cheerio')
-const $ = cheerio.load()
+app.use(express.static(path.join(__dirname, '/views')));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('index');
+});
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_database');
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// const cheerio = require('cheerio')
+// const $ = cheerio.load()
 
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI)
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/my_database');
+
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI)
+
+
+
+app.listen(PORT, function(){
+    console.log("Server listening on https://localhost:" + PORT)
+});
